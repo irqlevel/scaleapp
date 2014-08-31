deddf1647c54992d3cb22250ffbb3b3bd956fd3d
Andrey Smetanin <irqlevel@gmail.com>
2014-08-31 18:52:47 +0400

    1) added nginx load balancing config 2) added updating scaleapp from repo before run

---
+++ dockers/scaleapp/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /scaleapp && git pull
+cd /scaleapp && /apache-maven-3.2.3/bin/mvn package 
+cd /scaleapp && java -cp target/myapp-jar-with-dependencies.jar com.mycompany.myapp.App
+
