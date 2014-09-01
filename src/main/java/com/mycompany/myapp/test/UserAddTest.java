e8b4da1fb8dbb7e085c0fcd2088582bd5b35e83c
Andrey Smetanin <irqlevel@gmail.com>
2014-09-02 02:16:33 +0400

    increased test run

--- src/main/java/com/mycompany/myapp/test/UserAddTest.java
+++ src/main/java/com/mycompany/myapp/test/UserAddTest.java
@@ -5,21 +5,23 @@ import org.slf4j.LoggerFactory;
 
 import com.cserver.shared.HttpConn;
 import com.cserver.shared.SLog;
+import com.cserver.shared.Utils;
 import com.mycompany.myapp.UserAdd;
 
 public class UserAddTest {
     private static final Logger log = LoggerFactory.getLogger(ShardsUploadTest.class);
 	public static void main( String[] args ) {
 		SLog.startDefault();
-		
-		try {
-			UserAdd inf = new UserAdd();
-			inf.username = "petr";
-			
-			HttpConn.put("http", "0.0.0.0", 49153, "/user/add", inf.toString());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error("exception=", e);
+		for (int i = 0; i < 1000; i++) {
+			try {
+				UserAdd inf = new UserAdd();
+				inf.username = Utils.getRndString(8);
+
+				HttpConn.put("http", "0.0.0.0", 49153, "/user/add", inf.toString());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				log.error("exception=", e);
+			}
 		}
 	}
 }
