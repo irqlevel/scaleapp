8d48471281db11ec13af27aac601167344217988
Andrey Smetanin <irqlevel@gmail.com>
2014-09-08 16:09:21 +0400

    report app error

--- static/js/join.js
+++ static/js/join.js
@@ -6,6 +6,7 @@ joinApp.controller('joinCtrl',  ['$scope', '$http', function($scope, $http) {
 	};
 
 	$scope.joinUser = function() {
+		$scope.joinError = '';
 		console.log("email=" + $scope.email);
 		console.log("password=" + $scope.password);
 		$http.put('/user/join', JSON.stringify({username : $scope.email, password : $scope.password})).
@@ -13,12 +14,14 @@ joinApp.controller('joinCtrl',  ['$scope', '$http', function($scope, $http) {
 			function (data, status) {
 				console.log("data=" + data);
 				console.log("status=" + status);
+				if (data.error) 
+					$scope.joinError = data.errorS;
 			}
 		).
 		error(function (data, status) {
 			console.log("error data=" + data);
 			console.log("error status=" + status);
-			$scope.joinError = status;
+			$scope.joinError = "HTTP status " + status;
 		});
 	};
 }]);
